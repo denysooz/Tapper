89a45b4001eabc5662981328eb344dc35c3a3a0a
denysooz <den.darevskiy@mail.ru>
2024-06-05 16:52:03 +0300

    Initial Commit

---
+++ Tapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tapper
+//
+//  Created by Denis Dareuskiy on 5.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tapper/TapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapperApp.swift
+//  Tapper
+//
+//  Created by Denis Dareuskiy on 5.06.24.
+//
+
+import SwiftUI
+
+@main
+struct TapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
